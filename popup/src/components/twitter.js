b53693933834295359c783e4444912f11fe438cd
SergeKireev <cergyk.eth@gmail.com>
2022-07-14 12:52:29 +0200

    Simplify twitter button logic

--- popup/src/components/twitter.js
+++ popup/src/components/twitter.js
@@ -1,13 +1,8 @@
 export function displayTwitterButton(contractAddress, domain) {
     const safeDomain = domain.replaceAll('.', '[.]')
     const tweetText = encodeURI(
-        `@p33kablock\ncontract:${contractAddress.address}\ndomain:${safeDomain}\n#ScamReport`
+        `@p33kablock\ncontract:${contractAddress.address}\ndomain:${safeDomain}\n`
     );
     const twitterButton = document.getElementById('twitter-hashtag-button')
-    twitterButton.href = `${twitterButton.href}?text=${tweetText}`
-    
-    //We have to load the script after the link is fully formed
-    var script = document.createElement('script');
-    script.src = 'https://platform.twitter.com/widgets.js';
-    document.head.appendChild(script);
+    twitterButton.href = `${twitterButton.href}?text=${tweetText}&hashtags=ScamReport`
 }
